b37eb499be95136ee9780847ec66427e83b08ac4
Evandro <efraibo@gmail.com>
2020-10-08 01:14:49 -0300

    Ajustando ObjetoPersistente

--- src/main/java/com/zup/bootcamp/bootcampbank/entities/ObjetoPersistente.java
+++ src/main/java/com/zup/bootcamp/bootcampbank/entities/ObjetoPersistente.java
@@ -11,22 +11,16 @@ import org.springframework.data.domain.Persistable;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @Data
 @EqualsAndHashCode(of = {"id"})
 @MappedSuperclass
-public class ObjetoPersistente implements Persistable<Long> {
+public class ObjetoPersistente {
     @Id
-    @Column(name = "ID")
-    @GeneratedValue(generator = "increment")
-    @GenericGenerator(name = "increment", strategy = "increment")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    //Retorna se o Persistable novo ou já foi persistido.
-    @Override
-    public boolean isNew() {
-        return this.id == null;
-    }
 }
